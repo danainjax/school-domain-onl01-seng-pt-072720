0522c7c2ac41a68a7c06ef3075e7d9c94c90b261
Dana Pughakoff <danainjax@gmail.com>
2020-08-14 01:32:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+attr_accessor :school  
+
+def initialize(name)
+  @roster= {}
+  @name= name
+end
+
+def roster
+  @roster
+end
+
+def add_student(name, grade)
+  roster[grade] ||= []
+  roster[grade] << "#{name}"
+  
+end
+
+def grade(grade)
+  roster
+end
+
+
+end
